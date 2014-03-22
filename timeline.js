6a8d911fbad494e81317fa46916575bd051967df
Dawn <dawn_passerini@harvard.edu>
2014-03-22 13:09:24 -0400

    Stop animation frame working
    got stop animation frame to work on timeline and to stop when mouse out
    of timeline.

--- timeline.js
+++ timeline.js
@@ -16,9 +16,9 @@ var scrolling = false;
 window.onmousemove = function(event) {
 	mouseX = event.clientX;
 	mouseY = event.clientY;
-	if (!scrollTimeline) {
+	/*if (!scrollTimeline) {
 		scrollTimeline = window.setInterval(scroll, updateInterval);
-	}
+	}*/
 };
 
 // Global access to all the project li's
@@ -30,61 +30,72 @@ for (i = 0; i < projects.length; i++) {
 
 // Global access to the timeline container
 var timeline = document.querySelector("#timeline");
-//timeline.onmouseout = unslide; // Undo slides when out of timeline
+timeline.onmouseout = unslide; // Undo slides when out of timeline
 
-//variable for requestAnimationFrame
-/*var scrollFrame = null;*/
+// variable for requestAnimationFrame
+var scrollFrame = null;
 
-function scroll(event) {
+function scroll() {
+	scrollFrame = requestAnimationFrame(scroll);
 
+	//timeline.style.left = mouseX + 'px';
+
+	// orginal animation logic
 	var buffer = window.innerWidth/4;
 
 	var distanceToCenter = Math.abs(window.innerWidth/2-mouseX);
 	var speed = distanceToCenter/(window.innerWidth/2);
-
 	if (mouseX < buffer) {
+		console.log('left');
 		scrolling = true;
 		scrollLeft(speed);
 	}
 	else if ((window.innerWidth - mouseX) < buffer) {
+		console.log('right');
 		scrolling = true;
 		scrollRight(speed);
 	}
 	else {
 		scrolling = false;
-		window.clearInterval(scrollTimeline);
-		scrollTimeline = null;
+		// window.clearInterval(scrollTimeline);
+		// scrollTimeline = null;
 	}
 }
 
+function stopAnimation(){
+	cancelAnimationFrame(scrollFrame);
+}
+
+timeline.onmouseover = scroll;
+timeline.onmouseout = stopAnimation;
+
+/*var hero = document.getElementsByClassName('hero-overlay');
+
+hero.onmouseover = cancelAnimationFrame(scrollFrame);*/
+//window.setTimeout(function() { cancelAnimationFrame(scrollFrame); }, 20000);
 
 function scrollLeft(speed) {
 	scrollRight(speed*-1);
 }
 
 function scrollRight(speed) {
-
 	var leftPixels = parseInt(getStyleProp(timeline, 'left'), 10);
 	var toShift = Math.pow(speed,3)*updateInterval;
 	var newLeft = leftPixels - toShift;
 
-	if (newLeft >= -1400 && newLeft  <= 0) {
+	if (newLeft >= -1750 && newLeft  <= 0) {
 		timeline.style.left = newLeft + 'px';
 	}
 }
-
-timeline.onmouseover = myScrollFrame;
 //timeline.onmouseout = stopAnimation;
-
-var hero = document.getElementsByClassName('hero-overlay');
 
 function myScrollFrame(){
 	scrollFrame =  requestAnimationFrame(myScrollFrame);
 	timeline.style.left = mouseX + 'px';
-	hero.onmouseover = cancelAnimationFrame(myScrollFrame);
+	
 }
 
-
+//timeline.onmouseover = myScrollFrame;
 
 function slide(event)
 {
